3197b3540fa2a9cbcc5fbe6716d03d9412e5a087
Punit Goswami <pntgoswami18@gmail.com>
2019-09-08 09:31:05 +0530

    implemented bootstrap and jobs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Blog model
+# title
+# publication date
+# text blog post
+# image
+class Blog(models.Model):
+    title = models.CharField(max_length=120)
+    pub_date = models.DateField(auto_now_add=False)
+    text_body = models.TextField()
+    image = models.ImageField(upload_to='images/')
